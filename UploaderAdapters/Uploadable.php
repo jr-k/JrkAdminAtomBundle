358f5f56852644aa8c2bd08cd35e875cae2564f4
JRK <jrkdev@gmail.com>
2015-07-30 11:07:54 +0200

    fix trait classname issue

--- UploaderAdapters/Uploadable.php
+++ UploaderAdapters/Uploadable.php
@@ -8,7 +8,7 @@ trait Uploadable
 
 
     public function getUploadDirectoryPath($create = false) {
-        $dir = 'uploads/'.basename(str_replace('\\','/',strtolower(__CLASS__))).'-'.substr(md5(__NAMESPACE__),0,6).'/'.implode('/',str_split($this->getId()));
+        $dir = 'uploads/'.basename(str_replace('\\','/',strtolower(__CLASS__))).'-'.substr(md5(get_class($this)),0,6).'/'.implode('/',str_split($this->getId()));
 
         if (!is_dir($dir) && $create) {
             mkdir($dir,0777,true);
@@ -18,7 +18,7 @@ trait Uploadable
     }
 
     public function getResourceDirectoryPath() {
-        return 'resources/'.strtolower(__CLASS__).'-'.substr(md5(__NAMESPACE__),0,6);
+        return 'resources/'.strtolower(__CLASS__).'-'.substr(md5(get_class($this)),0,6);
     }
 
     public function getPicture()
